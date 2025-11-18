4201fa123ff4db3e99a71a970c42a8a4403e0753
xweoze <ww3777772@naver.com>
2025-11-18 19:43:42 +0900

    Initial Commit

---
+++ PetInformationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetInformationApp
+//
+//  Created by ㅏㅑㅡㅓㅑㅗㅕㅜ on 11/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetInformationApp/PetInformationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetInformationAppApp.swift
+//  PetInformationApp
+//
+//  Created by ㅏㅑㅡㅓㅑㅗㅕㅜ on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PetInformationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
